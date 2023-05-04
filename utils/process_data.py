196568d6d88bb34d48ee3d5e0907ead063a44fcd
Fabio Mantelli <fmmantelli@gmail.com>
2023-05-04 20:56:56 -0300

    feat: added unity tests

--- utils/process_data.py
+++ utils/process_data.py
@@ -1,24 +1,37 @@
-from utils.bad_status_flags import bad_status_flags
+from utils.good_status_flags import good_status_flags
 
 
 def process_data(my_dict):
     status_flags = []
     flag = False
 
-    for item in my_dict['TimeSeriesDataPoints']:
+    for i, item in enumerate(my_dict['TimeSeriesDataPoints']):
         value = item['Value']
-        filtered_value = bad_status_flags(value)
+        filtered_value = good_status_flags(value)
 
-        if (flag is False and filtered_value is True):
+        if (flag is False and filtered_value is False):
             status_flags.append(item)
             flag = True
 
-        elif (flag is True and filtered_value is False):
+            if i == len(my_dict['TimeSeriesDataPoints']) - 1:
+                status_flags.append(item)
+                return status_flags
+
+        elif (flag is True and filtered_value is True):
             status_flags.append(current_status_flags)
             flag = False
+        
         elif (flag is True and value != current_status_flags['Value']):
             status_flags.append(current_status_flags)
             status_flags.append(item)
 
+            if i == len(my_dict['TimeSeriesDataPoints']) - 1:
+                status_flags.append(item)
+                return status_flags
+        
+        elif (flag is True and i == len(my_dict['TimeSeriesDataPoints']) - 1):
+            status_flags.append(item)
+        
+        
         current_status_flags = item
     return status_flags
