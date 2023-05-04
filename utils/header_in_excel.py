67c06cf7664e7d6ab56d81c574c4d5eb8eef6a27
Fabio Mantelli <fmmantelli@gmail.com>
2023-05-04 15:46:52 -0300

    fix: coluns with accentuation

--- utils/header_in_excel.py
+++ utils/header_in_excel.py
@@ -4,10 +4,10 @@ def header_in_excel(worksheet, pmu):
     worksheet.cell(row=1, column=1, value=pmu)
 
     # Set the column titles in the second row
-    worksheet.cell(row=2, column=1, value='Numero')
+    worksheet.cell(row=2, column=1, value='Número')
     worksheet.cell(row=2, column=2, value='Data')
     worksheet.cell(row=2, column=3, value='Status Flags openPDC (hex)')
-    worksheet.cell(row=2, column=4, value='Status Flags openPDC (binario)')
-    worksheet.cell(row=2, column=5, value='Inicio (UTC)')
+    worksheet.cell(row=2, column=4, value='Status Flags openPDC (binário)')
+    worksheet.cell(row=2, column=5, value='Início (UTC)')
     worksheet.cell(row=2, column=6, value='Fim (UTC)')
-    worksheet.cell(row=2, column=7, value='Periodo')
+    worksheet.cell(row=2, column=7, value='Período')
