5a91cb12393c1d127296f995231194453fffec7e
Fabio Mantelli <fmmantelli@gmail.com>
2023-05-05 13:25:10 -0300

    feat: changed to accept many servers in ppa_status_flags model

--- status_flags.py
+++ status_flags.py
@@ -1,4 +1,5 @@
 import time
+import sys
 import requests
 import json
 from datetime import datetime, timedelta
@@ -7,6 +8,7 @@ from utils.process_data import process_data
 from utils.export_data_into_excel import export_data_into_excel
 from utils.create_excel_file import create_excel_file
 from utils.elapsed_time import elapsed_time
+from utils.get_servers import get_server
 
 from models.ppa_status_flags import ppa_status_flags
 
@@ -15,35 +17,41 @@ today = datetime.today()
 yesterday = today - timedelta(days=1)
 formatted_yesterday = yesterday.strftime("%m-%d-%y")
 
-server = '150.162.19.214'
-# startTime = f'{date} 18:45:00.000'
-# endTime = f'{date} 18:48:00.000'
-startTime = f'{formatted_yesterday} 00:00:00.000'
-endTime = f'{formatted_yesterday} 23:59:59.999'
+server_name = "ons_pdcmi_bsb"
+server = get_server(server_name)
 
-for item in ppa_status_flags:
-    start_time = time.time()
+if server is None:
+    print(f"There is no {server_name} in ppa_status_flags.")
+    sys.exit()
+else:
+    # startTime = f'{date} 18:45:00.000'
+    # endTime = f'{date} 18:48:00.000'
+    startTime = f'{formatted_yesterday} 00:00:00.000'
+    endTime = f'{formatted_yesterday} 23:59:59.999'
 
-    pmu = item['pmu']
-    statusFlags = item['statusFlags']
+    for item in ppa_status_flags[server_name]['ppa']:
+        start_time = time.time()
 
-    url = f'http://{server}:6152/historian/timeseriesdata/read/historic/{statusFlags}/{startTime}/{endTime}/json'
-    response = requests.get(url)
+        pmu = item['pmu']
+        statusFlags = item['statusFlags']
 
-    if response.status_code == 200:
-        data = response.text
-        my_dict = json.loads(data)
+        url = f'http://{server}:6152/historian/timeseriesdata/read/historic/{statusFlags}/{startTime}/{endTime}/json'
+        response = requests.get(url)
 
-        status_flags = process_data(my_dict)
-        print(f'[{pmu.upper()}] status_flags: {status_flags}')
-        create_excel_file(formatted_yesterday)
-        export_data_into_excel(status_flags, pmu, formatted_yesterday)
+        if response.status_code == 200:
+            data = response.text
+            my_dict = json.loads(data)
 
-    else:
-        print(f"Error: Failed to retrieve data from {url}")
+            status_flags = process_data(my_dict)
+            print(f'[{pmu.upper()}] status_flags: {status_flags}')
+            create_excel_file(formatted_yesterday)
+            export_data_into_excel(status_flags, pmu, formatted_yesterday, server_name)
 
-    end_time = time.time()
-    elapsed_time(start_time, end_time)
+        else:
+            print(f"Error: Failed to retrieve data from {url}")
 
-    # wait before making the next request
-    time.sleep(1)
+        end_time = time.time()
+        elapsed_time(start_time, end_time)
+
+        # wait before making the next request
+        time.sleep(1)
