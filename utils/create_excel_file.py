a3e10abfe879e23836341c0de7a79d321de4e300
Fabio Mantelli <fmmantelli@gmail.com>
2023-05-05 15:09:56 -0300

    fix: added server_name to create_excel_file() function

--- utils/create_excel_file.py
+++ utils/create_excel_file.py
@@ -1,13 +1,14 @@
 import os
 import shutil
 
-def create_excel_file(date):
+
+def create_excel_file(date, server_name):
     parts = date.split("-")
     # Extract the day and month values from the parts list
     year = parts[2]
     month = parts[0]
     # Rename the destination file
-    new_name = f"{year}_{month}_medfasee_status_flags.xlsx"
+    new_name = f"{year}_{month}_medfasee_{server_name}_status_flags.xlsx"
     new_path = os.path.join("./data/", new_name)
 
     if not os.path.exists(new_path):
