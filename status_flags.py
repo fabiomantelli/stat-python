210b0c955f222586340a9004263856c66d2087c4
Fábio Mantelli <fabiomantelli@fabiomacbookair.local>
2023-06-01 09:52:03 -0300

    fix: removed extra for

--- status_flags.py
+++ status_flags.py
@@ -19,19 +19,19 @@ today = datetime.today()
 yesterday = today - timedelta(days=1)
 formatted_yesterday = yesterday.strftime("%m-%d-%y")
 
-server_name = "ons_pdcmi_bsb"
+server_name = "brazil"
 server = get_server(server_name)
 
 if server is None:
     print(f"There is no {server_name} in ppa_status_flags.")
     sys.exit()
 else:
-    startTime = f'{date} 00:44:00.000'
-    endTime = f'{date} 00:45:00.000'
+    #startTime = f'{date} 00:44:00.000'
+    #endTime = f'{date} 00:45:00.000'
     #startTime = f'{date} 00:00:00.000'
-    #endTime = f'{date} 23:59:59.000'
-    #startTime = f'{formatted_yesterday} 00:00:00.000'
-    #endTime = f'{formatted_yesterday} 23:59:59.999'
+    #endTime = f'{date} 00:01:59.000'
+    startTime = f'{formatted_yesterday} 00:00:00.000'
+    endTime = f'{formatted_yesterday} 23:59:59.999'
 
     for item in ppa_status_flags[server_name]['ppa']:
         start_time = time.time()
@@ -48,9 +48,10 @@ else:
 
             status_flags = process_data(data_json)
             print(f'[{pmu.upper()}] status_flags: {status_flags}')
-            create_excel_file(date, server_name)
-            export_data_into_excel(
-                status_flags, pmu, date, server_name)
+            #create_excel_file(date, server_name)
+            #export_data_into_excel(
+            #    status_flags, pmu, date, server_name)
+            formatting(pmu, date, server_name)
 
         else:
             print(f"Error: Failed to retrieve data from {url}")
@@ -61,7 +62,7 @@ else:
         # wait before making the next request
         time.sleep(1)
 
-    for item in ppa_status_flags[server_name]['ppa']:
-        if 'pmu' in item:
-            pmu = item['pmu']
-            formatting(pmu, date, server_name)
+    #for item in ppa_status_flags[server_name]['ppa']:
+    #    if 'pmu' in item:
+    #        pmu = item['pmu']
+    #        formatting(pmu, date, server_name)
