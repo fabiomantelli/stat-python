d4173563add5073efcfae4881ecc39ed770e6a35
Fábio Mantelli <fabiomantelli@fabiomacbookair.local>
2023-06-04 22:26:06 -0300

    refactor: create new function to count the total duration of status flags

--- utils/excel_functions/get_total_duration_of_status_flags.py
+++ utils/excel_functions/get_total_duration_of_status_flags.py
@@ -1,9 +1,7 @@
 import datetime
 
-
 def get_total_duration_of_status_flags(worksheet):
     PERIOD_COLUMN = 7
-    TOTAL_PERIOD_COLUMN = 12
     total_duration = datetime.timedelta()
     for row in range(3, worksheet.max_row + 1):
         cell_value = worksheet.cell(row=row, column=PERIOD_COLUMN).value
@@ -14,4 +12,4 @@ def get_total_duration_of_status_flags(worksheet):
                                             seconds=time_value.second, microseconds=time_value.microsecond)
             total_duration += time_delta
 
-    worksheet.cell(row=3, column=TOTAL_PERIOD_COLUMN).value = total_duration
+    return total_duration
