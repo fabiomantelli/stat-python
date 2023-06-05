6614cd8b095ff86cbc28d564fcab19bfa39259b7
Fábio Mantelli <fabiomantelli@fabiomacbookair.local>
2023-06-04 22:28:45 -0300

    feat: test if get average period with two frequencies

---
+++ tests/test_get_average_period.py
@@ -0,0 +1,20 @@
+import unittest
+from unittest import mock
+from datetime import datetime, timedelta, time
+
+from utils.excel_functions.get_average_period import get_average_period
+
+class TestGetAveragePeriod(unittest.TestCase):
+
+    def test_get_average_period_if_has_two_frequencies(self):
+        mock_worksheet = mock.MagicMock()
+        average_period_cell = mock.MagicMock()
+        average_period_cell.value = timedelta(milliseconds=500)
+        
+        frequency_cell = mock.MagicMock()
+        frequency_cell.value = 2
+        mock_worksheet.cell.side_effect = [average_period_cell, frequency_cell]
+
+        result = get_average_period(mock_worksheet)
+
+        self.assertEqual(result, timedelta(milliseconds=250))
