13cb7860838e25565079b58fc34b79172785b44c
Fábio Mantelli <fabiomantelli@fabiomacbookair.local>
2023-06-07 11:44:38 -0300

    refactor: removed medfasee name from excel file

--- utils/excel_functions/create_excel_file.py
+++ utils/excel_functions/create_excel_file.py
@@ -3,11 +3,11 @@ import shutil
 
 def create_excel_file(date, server_name):
     month, day, year = date.split("-")
-    excel_file_name = f"{year}_{month}_medfasee_{server_name}_status_flags.xlsx"
+    excel_file_name = f"{year}_{month}_{server_name}_status_flags.xlsx"
     path = os.path.join("./data/", excel_file_name)
 
     if not os.path.exists(path):
-        source_path = "./data/model.xlsx"
+        source_path = f"./data/model_{server_name}.xlsx"
         destination_path = path
 
         shutil.copy(source_path, destination_path)
