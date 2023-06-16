144d60f7f6fe4d572d6ced1068e34f005e32e1e3
Fábio Mantelli <fabiomantelli@fabiomacbookair.local>
2023-06-15 22:50:18 -0300

    refactor: changed the name of the function

---
+++ tests/test_status_flags.py
@@ -0,0 +1,29 @@
+import unittest
+from app.utils.status_flags import status_flags
+
+class TestGoodStatusFlags(unittest.TestCase):
+    
+    def test_status_flags_if_data_error(self):
+        data = 557056
+        result = status_flags(data)
+        self.assertEqual(result, True)
+        
+    def test_status_flags_if_data_quality_sync(self):
+        data = 64
+        result = status_flags(data)
+        self.assertEqual(result, True)
+
+    def test_status_flags_if_data_ok(self):
+        data = 0
+        result = status_flags(data)
+        self.assertEqual(result, True)        
+
+    def test_status_flags_if_pmu_error(self):
+        data = 909312
+        result = status_flags(data)
+        self.assertEqual(result, True)
+
+    def test_status_flags_if_data_dif(self):
+        data = 909808
+        result = status_flags(data)
+        self.assertEqual(result, False)
