06a29244623fbbd1f68b634142f19535ab5c8957
Fábio Mantelli <fabiomantelli@fabiomacbookair.local>
2023-06-16 08:42:21 -0300

    fix: removed if verification of length of data_json

--- app/utils/process_data.py
+++ app/utils/process_data.py
@@ -5,7 +5,7 @@ def process_data(data_json):
     previous_status_flag = None
     previous_good_status_flag = True
     
-    for i, item in enumerate(data_json['TimeSeriesDataPoints']):
+    for item in data_json['TimeSeriesDataPoints']:
         good_status_flag = status_flags_handler(item['Value'])
         
         if previous_good_status_flag and not good_status_flag:
@@ -20,9 +20,8 @@ def process_data(data_json):
                 status_flags.append(item)
         
         previous_status_flag = item
-        
-        if i == len(data_json['TimeSeriesDataPoints']) - 1:
-            if not previous_good_status_flag:
-                status_flags.append(item)
+    
+    if not previous_good_status_flag:
+        status_flags.append(item)
     
     return status_flags
