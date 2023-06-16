a336640c63a92a93428733f2bd079a3c67306177
Fábio Mantelli <fabiomantelli@fabiomacbookair.local>
2023-06-15 22:50:36 -0300

    refactor: delete the olde good_status_flag

--- app/utils/good_status_flags.py
+++
@@ -1,6 +0,0 @@
-DATA_ERROR = 557056
-TIME_QUALITY_SYNC = 64
-PMU_ERROR = 909312
-
-def good_status_flags(status):
-    return (status == TIME_QUALITY_SYNC or status == DATA_ERROR or status == PMU_ERROR or status == 0)
